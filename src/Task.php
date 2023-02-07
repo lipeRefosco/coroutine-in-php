f44a73ee58b5e53eee21b7b85593862aabe831ed
lipeRefosco <luisfelipe.refosco@hotmail.com>
2023-02-07 12:56:58 -0300

    refact: create method to verify self state of Task

--- src/Task.php
+++ src/Task.php
@@ -10,21 +10,24 @@ final class Task
     private State $state;
     private Generator $generator;
 
-    function __construct(callable $generator)
+    function __construct(callable $generator, ...$params)
     {
         $this->state = State::Startup;
-        $this->generator = $generator();
+        $this->generator = $generator(...$params);
     }
 
-    public function resolve(): void
+    public function resolve(): State
     {
-        if($this->state == State::Startup){
+        if($this->isStarted()) { 
             $this->state = $this->current();
-            return;
+            return $this->state();
         }
-
-        $this->state = $this->next();
-        return;
+        
+        $this->isValid()
+            ? $this->next()
+            : $this->state = State::Done;
+            
+        return $this->state();
     }
 
     private function current(): State
@@ -34,15 +37,36 @@ final class Task
 
     private function next(): State
     {
-        $this->generator->next();
-        
-        $currentPositionIsValid = $this->generator->valid();
-
-        return $currentPositionIsValid ? State::Running : State::Done;
+        return $this->generator->next() ?? State::Running;
     }
 
     public function state(): State 
     {
         return $this->state;
+    }
+
+    public function isStarted(): bool
+    {
+        return $this->state == State::Startup;
+    }
+
+    public function isRunning(): bool
+    {
+        return $this->state == State::Running;
+    }
+
+    public function isPaused(): bool
+    {
+        return $this->state == State::Paused;
+    }
+
+    public function isDone(): bool
+    {
+        return $this->state == State::Done;
+    }
+
+    private function isValid(): bool
+    {
+        return $this->generator->valid();
     }
 }
